e5b26643409e0805021843c85ad7451e2174b71f
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2023-03-28 13:09:24 +0000

    Test in App.php

--- app/core/App.php
+++ app/core/App.php
@@ -11,9 +11,20 @@ class App
     public function __construct()
     {
         $url = $_SERVER['QUERY_STRING'];
+        if (!empty($url)) {
+            $url = trim($url, "/");
+            $url = explode("/", $url);
 
-        $url = explode("/", $url);
+            //define the controller
+            $this->controller = isset($url[0]) ? ucwords($url[0]) . "controller" : "HomeController";
 
-        echo $url[20] . "dvfvfgv";
+            //define the action
+            $this->action = isset($url[1]) ? $url[1] : "index";
+            unset($url[0], $url[1]);
+
+            $this->params = !empty($url) ? array_values($url) : [];
+
+            var_dump($this->params);
+        }
     }
 }
