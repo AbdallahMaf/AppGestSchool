8f2e5d50e4d31a1e18b31feee7aaf2e002b24eb1
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2023-04-09 13:59:57 +0000

    Error fixed

--- app/core/App.php
+++ app/core/App.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * 
+ * Front end controller
+ */
+
 class App
 {
     protected $controller = "HomeController";
@@ -10,21 +15,38 @@ class App
 
     public function __construct()
     {
+        $this->prepareURL();
+    }
+
+
+/**
+ * 
+ * Extract controller, method and parameters
+ * @return void
+ */
+
+
+    private function prepareURL()
+    {
         $url = $_SERVER['QUERY_STRING'];
         if (!empty($url)) {
             $url = trim($url, "/");
             $url = explode("/", $url);
 
             //define the controller
-            $this->controller = isset($url[0]) ? ucwords($url[0]) . "controller" : "HomeController";
+            $this->controller = isset($url[0]) ? ucwords($url[0])."controller":"HomeController";
 
             //define the action
             $this->action = isset($url[1]) ? $url[1] : "index";
             unset($url[0], $url[1]);
 
-            $this->params = !empty($url) ? array_values($url) : [];
+            $this->params = !empty($url) ? array_values($url):[];
 
             var_dump($this->params);
         }
+    }
+
+    private function render()
+    {
     }
 }
--- autoload.php
+++ autoload.php
@@ -1,13 +1,13 @@
 <?php
-define("DS", DIRECTORY_SEPARATOR);
-define("ROOT_PATH", dirname(__DIR__) . DS);
-define("APP", ROOT_PATH . 'APP' . DS);
-define("CORE", APP . 'Core' . DS);
-define("CONFIG", APP . 'Config' . DS);
-define("CONTROLLERS", APP . 'Controllers' . DS);
-define("MODELS", APP . 'Models' . DS);
-define("VIEWS", APP . 'Views' . DS);
-define("UPLOADS", ROOT_PATH . 'public' . DS . 'uploads' . DS);
+define("DS",DIRECTORY_SEPARATOR);
+define("ROOT_PATH",dirname(__DIR__).DS);
+define("APP",ROOT_PATH.'App'.DS);
+define("CORE",APP.'Core'.DS);
+define("CONFIG",APP.'Config'.DS);
+define("CONTROLLERS",APP.'Controllers'.DS);
+define("MODELS",APP.'Models'.DS);
+define("VIEWS",APP.'Views'.DS);
+define("UPLOADS",ROOT_PATH.'uploads'.DS);
 
 // configuration files 
 //require_once(CONFIG.'config.php');
@@ -16,8 +16,8 @@ define("UPLOADS", ROOT_PATH . 'public' . DS . 'uploads' . DS);
 
 
 // autoload all classes 
-$modules = [ROOT_PATH, APP, CORE, VIEWS, CONTROLLERS, MODELS, CONFIG];
-set_include_path(get_include_path() . PATH_SEPARATOR . implode(PATH_SEPARATOR, $modules));
-spl_autoload_register('spl_autoload', false);
+$modules = [ROOT_PATH,APP,CORE,VIEWS,CONTROLLERS,MODELS,CONFIG];
+set_include_path(get_include_path().PATH_SEPARATOR.implode(PATH_SEPARATOR,$modules));
+spl_autoload_register('spl_autoload',false);
 
 //new App();
